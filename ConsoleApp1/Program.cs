a9fbdb5a07c4441559497f62e4a70da9303039e0
thorbuckwell <thorbuckwell@gmail.com>
2022-02-07 11:03:30 -0500

    Add !!!!!

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("It Works!!");
+            Console.WriteLine("It Works!!!!!!!!");
             Console.ReadLine();
         }
     }
